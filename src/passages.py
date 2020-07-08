e72408444b13fe6af58707216932df62603d5157
Liam Moore <liam.moore.dev@gmail.com>
2020-07-08 17:31:14 +0000

    adds passages.py and creates passage and door classes

---
+++ src/passages.py
@@ -0,0 +1,10 @@
+class Passage:
+    def __init__(self, id, is_open, open_description, closed_description ):
+        self.id = id
+        self.is_open = is_open
+        self.open_description = open_description
+        self.closed_description = closed_description
+
+class Door(Passage):
+    def __init__(self, id, is_open, open_description = 'A wooden door.', closed_description = 'A locked wooden door.'):
+        super().__init__(id, is_open, open_description, closed_description)
