3060799c44671d301a15d18bc677cc78735a7154
Liam Moore <liam.moore.dev@gmail.com>
2020-07-08 18:18:33 +0000

    adds key item type

--- src/item.py
+++ src/item.py
@@ -3,3 +3,8 @@ class Item:
         self.name = name
         self.description = description
         self.id = id
+
+class Key(Item):
+    def __init__(self, name, description, id, locked_passage_id):
+        super().__init__(name, description, id)
+        self.locked_passage_id = locked_passage_id
--- src/items.py
+++ src/items.py
@@ -3,8 +3,9 @@ from item import Item
 # Declare all the rooms
 
 items = [
-    Item("Torch",
-                     "A wooden torch. It provides light.", 0),
+    Item("Torch","A wooden torch. It provides light.", 0),
+    Item("Rope","A long coil of sturdy rope.", 1),
+    Item("Iron Key","A polished iron key.", 2, 452)
 ]
 
 def getitemsbyids(ids):
